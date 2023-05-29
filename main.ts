242f6a2df488fb58d9e16ed48372c33d5ab7abda
Egyed József <jegyed50@gmail.com>
2023-05-29 07:34:38 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let intervalsms = 0
 // JEGYED50-BBC-MICROBIT-STUDY365-BLOCKS-03-03-C
 // Fish animation with create big image and scroll image
 let twohalffish = images.createBigImage(`
@@ -8,5 +9,6 @@ let twohalffish = images.createBigImage(`
     . . # . . . . . . .
     `)
 basic.forever(function () {
-    twohalffish.scrollImage(1, 100)
+    intervalsms = randint(80, 300)
+    twohalffish.scrollImage(1, intervalsms)
 })
